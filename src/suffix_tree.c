64f241efd17d6144bcaa2c07fbaa48b2a2272ed9
JonathanE42 <201804772@post.au.dk>
2022-11-28 00:22:35 +0100

    Refactor of suffix tree

--- src/suffix_tree.c
+++ src/suffix_tree.c
@@ -3,6 +3,19 @@
 #include <malloc.h>
 #include "suffix_tree.h"
 
+/*
+ * Follows an edge (Range r)
+ * Takes an integer current_matched_chars and returns the addition of characters on the edge
+ */
+int follow_edge(struct Range r, int current_matched_chars, const char *suffix, const char *str) {
+    for (int i = current_matched_chars; i < r.end; i++) {
+        // Break as soon we can't match any more on the edge
+        if (suffix[i] != str[r.start+i]) break;
+        current_matched_chars++;
+    }
+    return current_matched_chars;
+}
+
 void insert_node(struct SuffixTree *st, char *suffix, int suffix_len, char *str, int str_len) {
     // If we have not inserted anything yet, we would like to insert the suffix directly from the root
     struct SuffixTreeNode *place_to_insert = st->root;
@@ -18,63 +31,54 @@ void insert_node(struct SuffixTree *st, char *suffix, int suffix_len, char *str,
         if (matched_characters == suffix_len) break;
 
         // Check if the edge is matching characters in the suffix
-        // Todo: Refactor the for-loop to function "follow-edge" that returns (int: matched_characters)
-        for (int i = matched_characters; i < child->range.end; i++) {
-            if (suffix[i] == str[child->range.start+i]) {
-                matched_characters++;
-            } else {
-                // Early stopping on unmatched characters
-                break;
-            }
+        matched_characters = follow_edge(child->range, matched_characters, suffix, str);
+
+        // Check if we can not follow the edge. If not; check sibling.
+        if (!matched_characters) {
+            child = child->sibling;
+            continue;
         }
 
-        if (matched_characters) {
-            // We can follow the edge
-            if (matched_characters != child->range.end) {
-                // We did not match all characters; we should split
-
-                // steal the address of the current child which functions as a root in this subtree
-                struct SuffixTreeNode *subtree = child;
-
-                st->st_pool->next++;
-                struct SuffixTreeNode *fst_child = st->st_pool->next;
-                fst_child->range.start = str_len-suffix_len+matched_characters;
-                fst_child->range.end = str_len;
-                fst_child->leaf_label = str_len-suffix_len;
-                fst_child->parent = subtree;
-
-                st->st_pool->next++;
-                struct SuffixTreeNode *snd_child = st->st_pool->next;
-                fst_child->sibling = snd_child;
-
-                snd_child->parent = subtree;
-                snd_child->child = subtree->child;
-
-                //snd_child->sibling = subtree->sibling;
-
-                snd_child->leaf_label = subtree->leaf_label;
-                snd_child->range.start = subtree->range.start+matched_characters;
-                snd_child->range.end = subtree->range.end;
-
-                /*
-                 * Now as the split has been created, we have to clean up the edge from the
-                 * subtree to the subtree parent
-                 */
-                subtree->child = fst_child;
-                subtree->range.end = subtree->range.start+matched_characters;
-                subtree->leaf_label = -1;
-
-                return;
-            } else {
-                // We have matched all characters, so we should explore the child of the current child
-                place_to_insert = child;
-                child = child->child;
-                continue;
-            }
+        // If we have matched all characters, so we should explore the child of the current child
+        if (matched_characters == child->range.end) {
+            place_to_insert = child;
+            child = child->child;
+            continue;
         }
 
-        // Explore all siblings
-        child = child->sibling;
+        /*
+         * At this point we know, that we did not match all characters; we should split the edge
+         */
+
+        // steal the address of the current child which functions as a root in this subtree
+        struct SuffixTreeNode *subtree = child;
+
+        st->st_pool->next++;
+        struct SuffixTreeNode *fst_child = st->st_pool->next;
+        fst_child->range.start = str_len-suffix_len+matched_characters;
+        fst_child->range.end = str_len;
+        fst_child->leaf_label = str_len-suffix_len;
+        fst_child->parent = subtree;
+
+        st->st_pool->next++;
+        struct SuffixTreeNode *snd_child = st->st_pool->next;
+        fst_child->sibling = snd_child;
+
+        snd_child->parent = subtree;
+        snd_child->child = subtree->child;
+        snd_child->leaf_label = subtree->leaf_label;
+        snd_child->range.start = subtree->range.start+matched_characters;
+        snd_child->range.end = subtree->range.end;
+
+        /*
+         * Now as the split has been created, we have to clean up the edge from the
+         * subtree to the subtree parent
+         */
+        subtree->child = fst_child;
+        subtree->range.end = subtree->range.start+matched_characters;
+        subtree->leaf_label = -1;
+
+        return;
     }
 
     // Here we assume, that place_to_insert has been set correctly
@@ -92,6 +96,7 @@ void insert_node(struct SuffixTree *st, char *suffix, int suffix_len, char *str,
 
 struct SuffixTree *construct_st(char *str) {
     int n = (int) strlen(str);
+    // Edge case for when n is 1
     int num_of_nodes = n == 1 ? 2 : (2*n+1);
     struct SuffixTree *st = malloc(sizeof *st);
     struct SuffixTreeNodePool *st_pool = malloc(sizeof *st_pool);
